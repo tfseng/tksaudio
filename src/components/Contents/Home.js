d25a2445c8b09ff89b5df1aae190708f853c61d7
TEEN FONG <teenfong@pitchspot.co>
2021-10-22 18:04:57 +0800

    edited in Berklee Online to home page

--- src/components/Contents/Home.js
+++ src/components/Contents/Home.js
@@ -7,7 +7,7 @@ function Home(props) {
             <div className = "home-container">
                 <div>
                     <h1 className = "home-header">Hello, I'm Teen Kar!</h1>
-                    <p className = "home-text">I’m a junior year undergraduate student at Berklee College of Music, and an aspiring audio producer, sound designer and composer. Welcome to my Sound Design portfolio website.</p>
+                    <p className = "home-text">I’m a junior year undergraduate student at Berklee College of Music (Berklee Online), and an aspiring audio producer, sound designer and composer. Welcome to my Sound Design portfolio website.</p>
                     <div className = "home-resume"><a style={{ textDecoration: "underline" }}target = "blank"href = {Resume}>Download Resume</a></div> 
                 </div>
             </div>
